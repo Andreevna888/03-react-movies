585558c2ea29aad5a9aef21a485e257cf01df16d
tania <nominaomi888@gmail.com>
2026-09-12 22:00:36 +0300

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/03-react-movies/",
+  base: process.env.VERCEL ? "/" : "/03-react-movies/",
   plugins: [react()],
 });
